30e5869aa41ed18988aced5d1012ec83aa7e8602
Hsiaoming Yang <me@lepture.com>
2023-02-06 20:55:11 +0900

    feat(jwk): add thumbprint to generate kid

---
+++ src/joserfc/rfc7638/__init__.py
@@ -0,0 +1,19 @@
+import hashlib
+from collections import OrderedDict
+from .._types.keys import Key
+from .._util import to_bytes, json_dumps, urlsafe_b64encode
+
+
+def thumbprint(key: Key) -> str:
+    fields = list(key.required_fields)
+    fields.sort()
+
+    data = OrderedDict()
+    for k in fields:
+        data[k] = key.tokens[k]
+
+    json_data = json_dumps(data)
+    digest_data = hashlib.sha256(to_bytes(json_data)).digest()
+    kid = urlsafe_b64encode(digest_data).decode('utf-8')
+    key.kid = kid
+    return kid
