7adeba03aadd664944a81d94cf52efc4e3086a49
Hsiaoming Yang <me@lepture.com>
2023-02-12 12:10:06 +0900

    feat(jws): rfc8037 and rfc8812 algoritms for jws

--- src/joserfc/jws.py
+++ src/joserfc/jws.py
@@ -1,14 +1,33 @@
 from .rfc7517.keys import Key
+from .rfc7518.jws_algs import JWS_ALGORITHMS
+from .rfc8812 import ES256K
 from ._types.headers import SHeader
 from ._util import json_b64encode, urlsafe_b64encode
 
-JWS_REGISTRY = {}
+# supported algs
+JWS_REGISTRY = {alg.name: alg for alg in JWS_ALGORITHMS}
+JWS_REGISTRY[ES256K.name] = ES256K
+
+# predefined allowed algorithms
+DEFAULT_ALLOWED_ALGORITHMS = [
+]
 
 
-def serialize_compact(header: SHeader, payload: bytes, key: Key) -> bytes:
+def serialize_compact(
+    header: SHeader,
+    payload: bytes,
+    key: Key,
+    allowed_algorithms=None) -> bytes:
+
     assert key.is_private, "Private key is required to serialize the signature"
 
+    if allowed_algorithms is None:
+        allowed_algorithms = DEFAULT_ALLOWED_ALGORITHMS
+
     alg = header['alg']
+    if alg not in allowed_algorithms:
+        raise ValueError(f'Algorithm "{alg}" is not allowed in {allowed_algorithms}')
+
     algorithm = JWS_REGISTRY[alg]
 
     protected_segment = json_b64encode(header)
@@ -20,11 +39,12 @@ def serialize_compact(header: SHeader, payload: bytes, key: Key) -> bytes:
     return b'.'.join([protected_segment, payload_segment, signature])
 
 
-def extract_compact(text: str):
-    pass
+def extract_compact(text: str, allowed_algorithms=None):
+    if allowed_algorithms is None:
+        allowed_algorithms = DEFAULT_ALLOWED_ALGORITHMS
 
 
-def deserialize_compact(text: str, key: Key):
+def deserialize_compact(text: str, key: Key, allowed_algorithms=None):
     pass
 
 
