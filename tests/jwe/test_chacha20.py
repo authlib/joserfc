eb86cb9ad784094cf50b62e106a04c34c312456f
Hsiaoming Yang <me@lepture.com>
2023-06-19 23:26:40 +0900

    chore: add more test cases for jwe

--- tests/jwe/test_chacha20.py
+++ tests/jwe/test_chacha20.py
@@ -2,10 +2,11 @@ from unittest import TestCase
 from joserfc.jwe import (
     encrypt_compact,
     decrypt_compact,
-    JWERegistry
+    JWERegistry,
+    EncryptionData,
 )
 from joserfc.jwk import OctKey
-from joserfc.drafts.jwe_chacha20 import JWE_ENC_MODELS
+from joserfc.drafts.jwe_chacha20 import JWE_ENC_MODELS, XC20P
 
 
 class TestChaCha20(TestCase):
@@ -36,3 +37,37 @@ class TestChaCha20(TestCase):
 
     def test_dir_xc20p(self):
         self.run_test_dir("XC20P")
+
+    def test_xc20p_content_encryption_decryption(self):
+        # https://datatracker.ietf.org/doc/html/draft-irtf-cfrg-xchacha-03#appendix-A.3.1
+        payload = bytes.fromhex(
+            '4c616469657320616e642047656e746c656d656e206f662074686520636c6173' +
+            '73206f66202739393a204966204920636f756c64206f6666657220796f75206f' +
+            '6e6c79206f6e652074697020666f7220746865206675747572652c2073756e73' +
+            '637265656e20776f756c642062652069742e'
+        )
+
+        obj = EncryptionData({}, payload)
+        obj.cek = bytes.fromhex('808182838485868788898a8b8c8d8e8f909192939495969798999a9b9c9d9e9f')
+        aad = bytes.fromhex('50515253c0c1c2c3c4c5c6c7')
+        obj.encoded["aad"] = aad
+        iv = bytes.fromhex('404142434445464748494a4b4c4d4e4f5051525354555657')
+        obj.decoded["iv"] = iv
+
+        ciphertext = XC20P.encrypt(obj)
+        self.assertEqual(
+            ciphertext,
+            bytes.fromhex(
+                'bd6d179d3e83d43b9576579493c0e939572a1700252bfaccbed2902c21396cbb' +
+                '731c7f1b0b4aa6440bf3a82f4eda7e39ae64c6708c54c216cb96b72e1213b452' +
+                '2f8c9ba40db5d945b11b69b982c1bb9e3f3fac2bc369488f76b2383565d3fff9' +
+                '21f9664c97637da9768812f615c68b13b52e'
+            )
+        )
+
+        tag = obj.decoded["tag"]
+        self.assertEqual(tag, bytes.fromhex('c0875924c1c7987947deafd8780acf49'))
+
+        obj.decoded["ciphertext"] = ciphertext
+        plaintext = XC20P.decrypt(obj)
+        self.assertEqual(plaintext, payload)
--- tests/jwe/test_compact.py
+++ tests/jwe/test_compact.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 from joserfc.jwe import encrypt_compact, decrypt_compact, JWERegistry
-from joserfc.jwk import RSAKey, ECKey
+from joserfc.jwk import RSAKey, ECKey, OctKey
 from joserfc.rfc7518.jwe_encs import JWE_ENC_MODELS
 from tests.util import read_key
 
@@ -28,15 +28,36 @@ class TestJWECompact(TestCase):
             for enc in JWE_ENC_MODELS:
                 self.run_case(alg, enc.name, private_key, public_key)
 
-    def test_with_rsa_key(self):
+    def test_RSA_alg(self):
         private_key = RSAKey.import_key(read_key('openssl-rsa-private.pem'))
         public_key = RSAKey.import_key(read_key('openssl-rsa-public.pem'))
         algs = ['RSA1_5', 'RSA-OAEP', 'RSA-OAEP-256']
         self.run_cases(algs, private_key, public_key)
 
-    def test_with_ec_key(self):
+    def test_ECDH_ES_alg(self):
         algs = ['ECDH-ES', 'ECDH-ES+A128KW', 'ECDH-ES+A192KW', 'ECDH-ES+A256KW']
         for size in [256, 384, 512]:
             private_key = ECKey.import_key(read_key(f'ec-p{size}-private.pem'))
             public_key = ECKey.import_key(read_key(f'ec-p{size}-public.pem'))
             self.run_cases(algs, private_key, public_key)
+
+    def test_dir_alg(self):
+        self.assertRaises(ValueError, encrypt_compact, {"alg": "dir", "enc": "A128GCM"}, b"j", "secret")
+        for enc in JWE_ENC_MODELS:
+            key = OctKey.generate_key(enc.cek_size)
+            self.run_case('dir', enc.name, key, key)
+
+    def test_AESGCM_alg(self):
+        for size in [128, 192, 256]:
+            key = OctKey.generate_key(size)
+            self.run_cases([f"A{size}GCMKW"], key, key)
+
+    def test_PBES2HS_alg(self):
+        algs = {
+            "PBES2-HS256+A128KW": 128,
+            "PBES2-HS384+A192KW": 192,
+            "PBES2-HS512+A256KW": 256,
+        }
+        for alg in algs:
+            key = OctKey.generate_key(algs[alg])
+            self.run_cases([alg], key, key)
